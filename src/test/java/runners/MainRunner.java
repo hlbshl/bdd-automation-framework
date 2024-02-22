5b151e5884660c9fa8dbd5eb67e2d5b5d911a634
Hleb Shulhin <hlebshulhin@gmail.com>
2024-02-22 17:33:10 +0300

    Initial commit

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,15 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"}, monochrome = true, dryRun = false,
+        tags = "", plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
+public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
